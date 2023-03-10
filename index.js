198e0144de91f889f1c828c9c16bfaf057bad412
Ulises <ulises.argueta95@gmail.com>
2023-03-10 13:45:36 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+};
+
+function appendCat(name) {
+    const newCats = [...cats, "Broom"]
+    return newCats;
+};
+
+function prependCat(name) {
+    const newCats = ["Arnold", ...cats]
+    return newCats
+};
+
+function removeLastCat() {
+    const newCats2 = cats.slice(0, 2)
+    return newCats2
+};
+
+function removeFirstCat() {
+    const newCats2 = cats.slice(1)
+    return newCats2
+};
